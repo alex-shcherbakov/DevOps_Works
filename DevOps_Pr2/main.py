1b94759418577e01d07db5e7fb3c998e725f23b8
alex-shcherbakov <alex.shcherbakov.2@gmail.com>
2025-09-26 09:24:00 +0300

    дороблення коду main

--- DevOps_Pr2/main.py
+++ DevOps_Pr2/main.py
@@ -1,5 +1,6 @@
 from JsonReader import JsonReader
 from CsvToJsonConverter import CsvToJsonConverter
+from StudentFinder import StudentFinder
 
 if __name__ == "__main__":
     csv_url = "https://informer.com.ua/dut/python/import/st_gt.csv"
@@ -12,3 +13,20 @@ if __name__ == "__main__":
     # Читання та відображення даних з JSON
     json_reader = JsonReader()
     json_reader.display_data(json_path)
+    csv_url = "https://informer.com.ua/dut/python/import/st_gt.csv"
+    json_path = "students_data.json"  # Шлях до файлу JSON
+
+    # Конвертація з CSV до JSON
+    converter = CsvToJsonConverter()
+    converter.read_and_convert(csv_url, json_path)
+
+    # Створення об'єкта JsonReader та виклик методу для читання та відображення даних з JSON
+    json_reader = JsonReader()
+    # json_reader.display_data(json_path)
+
+    # Пошук та виведення інформації про студентів
+    finder = StudentFinder(json_reader)
+    finder.load_data(json_path)
+    surname_to_find = "Барченко"  # Вкажіть прізвище для пошуку
+    found_students = finder.find_students_by_surname(surname_to_find)
+    finder.display_students_info(found_students)
