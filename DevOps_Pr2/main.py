422bdd260206e7f4d095342e2bd95b004ffcd84c
alex-shcherbakov <alex.shcherbakov.2@gmail.com>
2025-09-26 09:22:19 +0300

    доробив код для JSON

--- DevOps_Pr2/main.py
+++ DevOps_Pr2/main.py
@@ -1,8 +1,14 @@
-from CsvReader import CsvReader
+from JsonReader import JsonReader
+from CsvToJsonConverter import CsvToJsonConverter
 
 if __name__ == "__main__":
     csv_url = "https://informer.com.ua/dut/python/import/st_gt.csv"
-    csv_reader = CsvReader()
-    data = csv_reader.read_data(csv_url)
-    for row in data:
-        print(row)
+    json_path = "students_data.json"  # Шлях до файлу JSON
+
+    # Ініціалізація CsvToJsonConverter та виконання конвертації
+    converter = CsvToJsonConverter()
+    converter.read_and_convert(csv_url, json_path)
+
+    # Читання та відображення даних з JSON
+    json_reader = JsonReader()
+    json_reader.display_data(json_path)
