44ff74774ed0055b74439f4ac2649bb00db207f4
alex-shcherbakov <alex.shcherbakov.2@gmail.com>
2025-11-13 13:06:29 +0100

    Створені три маршрути для flask

--- venv/app.py
+++ venv/app.py
@@ -1,12 +1,39 @@
-from http.cookiejar import debug
-
-from flask import Flask
+from flask import Flask, jsonify
+import time
 
 app = Flask(__name__)
 
+start_time = time.time()
+
+request_count = 0
+
+
+@app.before_request
+def before_request():
+    global request_count
+    request_count += 1
+
+
 @app.route("/")
 def home():
-    return "Hello for QA from Flask"
+    return "Сервіс працює"
 
-if __name__=="__main__":
+# викликає помилку
+@app.route("/error")
+def error():
+    return 1 / 0  # ZeroDivisionError
+
+# повертає JSON
+@app.route("/status")
+def status():
+    uptime = round(time.time() - start_time, 2)
+    data = {
+        "status": "ok",
+        "uptime_seconds": uptime,
+        "requests_handled": request_count
+    }
+    return jsonify(data)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
