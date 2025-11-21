54e4f45b264e3ff84bb98db3c587e60277b8e069
alex-shcherbakov <alex.shcherbakov.2@gmail.com>
2025-11-21 07:43:59 +0100

    Додав посторінкову розбивку

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,8 +1,14 @@
 from .models import Post
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     posts = Post.published.all()
+    # Пагінація з 3 постами на сторінку
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
+
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, year, month, day, post):
