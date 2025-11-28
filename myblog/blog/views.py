396e8d368ed2f699be9e12a9ac1037f3ca9e6deb
alex-shcherbakov <alex.shcherbakov.2@gmail.com>
2025-11-28 18:51:02 +0100

    Фільтрація постів і схожих статей за тегами

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -7,6 +7,8 @@ from .models import Post, Comment
 from .forms import CommentForm
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
+
 
 
 
@@ -43,6 +45,12 @@ def post_detail(request, year, month, day, post):
 
     # Форма для коментування користувачами
     form = CommentForm()
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids) \
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
+                        .order_by('-same_tags', '-publish')[:4]
 
     return render(request, 'blog/post/detail.html',
                   {'post': post,
