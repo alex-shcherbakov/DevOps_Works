222d69ef9dbe673f3a3146cb47744ebaabf791e3
alex-shcherbakov <alex.shcherbakov.2@gmail.com>
2025-11-28 18:42:43 +0100

    Відображення тегів у шаблоні Django

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -6,13 +6,18 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Comment
 from .forms import CommentForm
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
 
 
 
 
-
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
     try:
@@ -22,7 +27,7 @@ def post_list(request):
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
 
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, 'blog/post/list.html', {'posts': posts, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
