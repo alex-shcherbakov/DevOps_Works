5c26e72587716e3fd04ca0ee97503ad3ad1278de
alex-shcherbakov <alex.shcherbakov.2@gmail.com>
2025-11-28 21:13:42 +0100

    Пошук створений

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from . import views
-from django.contrib.sitemaps import Sitemap
 from django.contrib.sitemaps.views import sitemap
 from .sitemaps import PostSitemap
 from .feeds import LatestPostsFeed
-
 
 sitemaps = {
     'posts': PostSitemap,
@@ -21,9 +19,7 @@ urlpatterns = [
     path('post/<int:post_id>/share/', views.post_share, name='post_share'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
     path('feed/', LatestPostsFeed(), name='post_feed'),
     path('search/', views.post_search, name='post_search'),
-
-
 ]
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -8,6 +8,9 @@ from taggit.models import Tag
 from django.db.models import Count
 from django.contrib.postgres.search import SearchVector
 from .forms import EmailPostForm, CommentForm, SearchForm
+from django.contrib.postgres.search import SearchVector, \
+SearchQuery, SearchRank
+
 
 def post_search(request):
     form = SearchForm()
@@ -21,6 +24,12 @@ def post_search(request):
             results = Post.published.annotate(
                 search=SearchVector('title', 'body'),
             ).filter(search=query)
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
     return render(request,
                   'blog/post/search.html',
                   {'form': form,
