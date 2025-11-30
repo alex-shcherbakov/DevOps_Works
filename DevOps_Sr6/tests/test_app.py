80802926edcc9b3440538d1634c49a9fff106852
alex-shcherbakov <alex.shcherbakov.2@gmail.com>
2025-11-30 12:24:06 +0100

    Додатна проста фікстура

--- DevOps_Sr6/tests/test_app.py
+++ DevOps_Sr6/tests/test_app.py
@@ -1,5 +1,4 @@
 import pytest
-
 from DevOps_Sr6.app import add, is_even
 
 
@@ -9,13 +8,26 @@ def test_add_simple():
     assert add(10, -5) == 5
 
 
-@pytest.mark.parametrize("value, expected", [
-    (2, True),
-    (3, False),
-    (10, True),
-    (0, True),
-    (-4, True),
-    (-3, False),
-])
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        (2, True),
+        (3, False),
+        (10, True),
+        (0, True),
+        (-4, True),
+        (-3, False),
+    ],
+)
 def test_is_even(value, expected):
     assert is_even(value) == expected
+
+# Фікстура
+@pytest.fixture
+def sample_numbers():
+    return [2, 4, 6]
+
+
+def test_all_even(sample_numbers):
+    for n in sample_numbers:
+        assert is_even(n)
